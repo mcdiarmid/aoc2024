1e6377d23e29e68e3819409f5554ff237dded301
mcdiarmid <campbell.mcdiarmid@icloud.com>
2024-12-29 21:19:51 +1300

    Day2 27 days late!

--- src/main.rs
+++ src/main.rs
@@ -65,6 +65,61 @@ fn day1(lines: &Vec<String>, a: bool) -> Result<String> {
     Ok(result.to_string())
 }
 
+
+fn day2(lines: &Vec<String>, a: bool) -> Result<String> {
+    fn is_generally_ascending(v: &Vec<i32>) -> bool {
+        let integral = v.windows(2)
+            .map(|w| (w[1] > w[0]) as i32 * 2 - 1)
+            .into_iter()
+            .reduce(|a, b| a + b);
+        return integral.unwrap() > 0;
+    }
+
+    fn is_safe_pair(a: &i32, b: &i32, ascending: bool) -> bool {
+        let delta = if ascending {*b - *a} else {*a - *b};
+        (1..=3).contains(&delta)
+    }
+
+    fn first_intolerable(v: &Vec<i32>, ascending: bool) -> Option<usize> {
+        for i in 0..v.len() - 1 {
+            if !is_safe_pair(&v[i], &v[i+1], ascending) {
+                let first_pair_safe = i == 0 || is_safe_pair(&v[i-1], &v[i+1], ascending);
+                let second_pair_safe = i < v.len() - 2 && is_safe_pair(&v[i+1], &v[i+2], ascending);
+                return if first_pair_safe && second_pair_safe { Some(i) } else { Some(i+1) };
+            }
+        }
+        None
+    }
+
+    let mut num_safe = 0;
+
+    for i in 0..lines.len() {
+        let mut parsed: Vec<i32> = lines[i]
+            .split_ascii_whitespace()
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<i32>>();
+
+        // Determine if we're generally ascending or descending
+        let ascending = is_generally_ascending(&parsed);
+
+        // Remove the first intolerable level (if one exists)
+        if !a {
+            if let Some(index) = first_intolerable(&parsed, ascending) {
+                parsed.remove(index);
+            }
+        }
+
+        // Check if line is safe
+        if parsed.windows(2).all(|w| is_safe_pair(&w[0], &w[1], ascending))
+        {
+            num_safe += 1;
+        }
+    }
+
+    Ok(num_safe.to_string())
+}
+
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -91,6 +146,20 @@ mod test {
         assert_eq!(day1(&input, true).unwrap(), "11");
         assert_eq!(day1(&input, false).unwrap(), "31");
     }
+
+    #[test]
+    fn test_day2() {
+        let input_str = r"
+        7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9";
+        let input = input_str_to_vec_of_strings(&input_str);
+        assert_eq!(day2(&input, true).unwrap(), "2");
+        assert_eq!(day2(&input, false).unwrap(), "4");
+    }
 }
 
 fn main() -> Result<()> {
@@ -100,6 +169,7 @@ fn main() -> Result<()> {
     // Get thr function or raise before any reading or parsing
     let aoc_fn: AocFn = match cli.day {
         1 => day1,
+        2 => day2,
         _ => Err("Not Implemented.")?
     };
 
